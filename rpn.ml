5e9ee7277c3da3936d27940757f5404920716ddc
mirdaki <mirdaki@users.noreply.github.com>
2019-02-20 11:07:48 -0500

    Update for second revision
    Using code from in-class demo with original stuff

--- rpn.ml
+++ rpn.ml
@@ -1,60 +1,82 @@
-open Str
+(* Defintion for input items *)
+type token =
+  | Number of float
+  | Operator of string
+  | TokenError of string
+;;
+
+(* Convert a single string into a token *)
+let to_token (input: string) : token =
+  match input with
+    | ("+"|"-"|"*"|"/"|"^") -> Operator(input)
+    | _ -> let value = float_of_string_opt(input) in
+      match value with
+        | Some(num) -> Number(num)
+        | None -> TokenError("Unrecognized token '" ^ input ^ "'")
+;;
 
 (* Run basic mathmatical operations *)
-let do_math operator first_operand second_operand = 
+let do_math (operator: string) (operand1: float) (operand2: float) = 
   match operator with
-    | "+" -> Ok (first_operand +. second_operand)
-    | "-" -> Ok (first_operand -. second_operand)
-    | "*" -> Ok (first_operand *. second_operand)
+    | "+" -> Ok(operand1 +. operand2)
+    | "-" -> Ok(operand1 -. operand2)
+    | "*" -> Ok(operand1 *. operand2)
     | "/" -> 
-        if (second_operand = 0.) then
-          Error ("Divide by zero")
-        else 
-          Ok (first_operand /. second_operand)
-    | "^" -> Ok (first_operand ** second_operand)
-    | _ -> Error ("Invalid operator")
-;;
-
-(* Recrusively find the value or a reverse polish notation *)
-let rec rpn input_list storage =
-  match input_list with
-    | [] -> 
-      if Stack.length storage = 1 then
-        Ok (Stack.pop storage)
+      if (operand2 = 0.) then
+        Error("Divide by zero")
       else 
-        Error ("Invalid input")
-    | hd -> 
-      match List.hd input_list with
-        | ("+"|"-"|"*"|"/"|"^") -> 
-          begin
-            if Stack.length storage >= 2 then
-              match do_math (List.hd input_list) (Stack.pop storage) (Stack.pop storage) with
-              | Ok output -> 
-                Stack.push output storage;
-                rpn (List.tl input_list) storage
-              | Error output -> Error (output)
-            else
-              Error ("Not enough arguments")
-          end
-        | _ -> 
-          match float_of_string_opt (List.hd input_list) with
-          | Some output-> 
-            Stack.push output storage;
-            rpn (List.tl input_list) storage
-          | None -> Error ("Not a number")
+        Ok(operand1 /. operand2)
+    | "^" -> Ok(operand1 ** operand2)
+    | _ -> Error("Invalid operator")
 ;;
 
-(* Split an input string on spaces *)
-let split_str raw_input =
-  Str.split (Str.regexp " +") raw_input
+(* The results from the RPN evaluation *)
+type stackState =
+  | Stack of float list
+  | StackError of string
 ;;
 
-(* Print the output of rpn on a new line *)
-let evaluate_line input = 
-  match (rpn (split_str input) (Stack.create ())) with
-    | Ok output -> string_of_float output
-    | Error output -> output
+(* Do RPN evaluation for a single item and stack *)
+let next_state (state: stackState) (index: token) : stackState =
+  match state with
+    | StackError(err) -> state
+    | Stack(lst) ->
+      match index with
+        | Number(flt) -> Stack(flt::lst)
+        | Operator(op) -> (
+          match lst with
+            | operand2::operand1::tail -> (
+              match do_math op operand1 operand2 with 
+                | Ok(num) -> Stack(num::tail)
+                | Error(err) -> StackError(err)
+            )
+            | _ -> StackError("Not enough arguments for '" ^ op ^ "'")
+          )
+        | TokenError(err) -> StackError(err)
+;;
+
+(* Convert the final RPN output to a string *)
+let result_to_string (input: stackState) : string = 
+  match input with
+    | Stack(lst) -> (
+      match lst with
+      | hd::[] -> string_of_float hd
+      | _ -> "Not enough operators"
+    )
+    | StackError(output) -> output
+;;
+
+(* Connect all of the RPN functions together to do the RPN evaluation *)
+let process_rpn (input: string) =
+  input |> (* Get the input *)
+  String.split_on_char ' ' |> (* Break up the input on spaces *)
+  List.map to_token |> (* Make the broken up string into a token list *)
+  List.fold_left next_state (Stack[]) |> (* Find the stack processing the input left-to-right *)
+  result_to_string (* result into string *)
 ;;
 
 (* Get rpn from stdin and output result *)
-print_endline (evaluate_line (read_line ()));;
+read_line () |> 
+process_rpn |>
+print_endline
+;;
--- test.ml
+++ test.ml
@@ -4,29 +4,29 @@ open OUnit
 let test_fixture = "rpn" >:::
 [
   "correct" >:: ( fun () -> 
-    assert_equal "1." (Rpn.evaluate_line "1");
-    assert_equal "2." (Rpn.evaluate_line "1 1 +");
-    assert_equal "0.5" (Rpn.evaluate_line "1.5 1 -");
-    assert_equal "9." (Rpn.evaluate_line "3 3 *");
-    assert_equal "1." (Rpn.evaluate_line "3 3 /");
-    assert_equal "27." (Rpn.evaluate_line "3 3 ^");
-    assert_equal "3.14285714286" (Rpn.evaluate_line "22 7 /");
-    assert_equal "0." (Rpn.evaluate_line "1 2 + 3 -");
-    assert_equal "18." (Rpn.evaluate_line "2 3 11 + 5 - *");
-    assert_equal "1." (Rpn.evaluate_line "9 5 3 + 2 4 ^ - +");
-    assert_equal "2." (Rpn.evaluate_line "162 2 1 + 4 ^ / ");
-    assert_equal "5." (Rpn.evaluate_line "15 7 1 1 + - / 3 * 2 1 1 + + -");
+    assert_equal "1." (Rpn.process_rpn "1");
+    assert_equal "2." (Rpn.process_rpn "1 1 +");
+    assert_equal "0.5" (Rpn.process_rpn "1.5 1 -");
+    assert_equal "9." (Rpn.process_rpn "3 3 *");
+    assert_equal "1." (Rpn.process_rpn "3 3 /");
+    assert_equal "27." (Rpn.process_rpn "3 3 ^");
+    assert_equal "3.14285714286" (Rpn.process_rpn "22 7 /");
+    assert_equal "0." (Rpn.process_rpn "1 2 + 3 -");
+    assert_equal "18." (Rpn.process_rpn "2 3 11 + 5 - *");
+    assert_equal "1." (Rpn.process_rpn "9 5 3 + 2 4 ^ - +");
+    assert_equal "2." (Rpn.process_rpn "162 2 1 + 4 ^ /");
+    assert_equal "5." (Rpn.process_rpn "15 7 1 1 + - / 3 * 2 1 1 + + -");
   );
 
   "error" >:: ( fun () ->
-    assert_equal "Invalid input" (Rpn.evaluate_line "");
-    assert_equal "Not enough arguments" (Rpn.evaluate_line "-");
-    assert_equal "Not a number" (Rpn.evaluate_line "1 q -");
-    assert_equal "Divide by zero" (Rpn.evaluate_line "1 0 /");
-    assert_equal "Divide by zero" (Rpn.evaluate_line "0 0 /");
-    assert_equal "Not enough arguments" (Rpn.evaluate_line "1 +");
-    assert_equal "Invalid input" (Rpn.evaluate_line "1 1 11 1 1 +");
-    assert_equal "Not a number" (Rpn.evaluate_line "hello");
+    assert_equal "Unrecognized token ''" (Rpn.process_rpn "");
+    assert_equal "Not enough arguments for '-'" (Rpn.process_rpn "-");
+    assert_equal "Unrecognized token 'q'" (Rpn.process_rpn "1 q -");
+    assert_equal "Divide by zero" (Rpn.process_rpn "1 0 /");
+    assert_equal "Divide by zero" (Rpn.process_rpn "0 0 /");
+    assert_equal "Not enough arguments for '+'" (Rpn.process_rpn "1 +");
+    assert_equal "Not enough operators" (Rpn.process_rpn "1 1 11 1 1 +");
+    assert_equal "Unrecognized token 'hello'" (Rpn.process_rpn "hello");
   );
 ]
 
